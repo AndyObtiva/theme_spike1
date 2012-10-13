3afd0f9f79d2a762bdbc80d4df33ef147ca1f388
Andy Maleh <AndyMaleh@Andys-MacBook-Air.local>
2012-10-13 17:46:05 -0400

    Initial commit of theme spike 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThemeSpike1::Application.config.secret_token = 'a2af0a6678a52c062bd377b0f89d0af85a108dcafab9e4cb1f065b03bdfeab4870cd6ef85154427a0b37edb4cade5c698623fe6c869667645a2d8e6d60345fad'
